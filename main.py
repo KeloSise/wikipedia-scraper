3f6dca0b60bd95c5319177006a1a036c8a454c7d
KeloSise <s.sinyolo@gmail.com>
2024-02-02 17:20:30 +0100

    Added code to main.py to interact with scraper.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+from src.scraper import WikipediaScraper
+
+def main():
+    scraper = WikipediaScraper()
+    countries = scraper.get_countries()
+    print(countries)
+
+    leader = scraper.get_leaders(countries)
+    print([leader])
+
+if __name__ == "__main__":
+    main()
