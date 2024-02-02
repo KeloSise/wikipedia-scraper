1166c03c1134581b5464faf3cff551c3852eecc4
KeloSise <s.sinyolo@gmail.com>
2024-02-02 23:30:47 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     countries = scraper.get_countries()
     print(countries)
 
-    leader = scraper.get_leaders(countries)
-    print([leader])
+    leader_data = scraper.get_leaders(countries)
+    print(leader_data)
 
 if __name__ == "__main__":
     main()
