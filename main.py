754be4c5c6a401a8619dbe0bb1e5f23ac05daa52
KeloSise <s.sinyolo@gmail.com>
2024-02-05 01:35:53 +0100

    Updated main.py to correctly print leader data

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def main():
     print(countries)
 
     leader_data = scraper.get_leaders(countries)
-    print(leader_data)
+    scraper.print_leaders(leader_data)
+
+    leader_output_filepath = "leader_output.json"
+    scraper.to_json_file(leader_output_filepath)
+    print(f"Updated leaders data saved to {leader_output_filepath}")
+
 
 if __name__ == "__main__":
     main()
